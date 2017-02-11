27ca81e69183808eca787dddf8b8726fd80250ef
Sergey <sabarovsky@yahoo.com>
2017-02-11 23:03:31 +0200

    To understand the number of unique fields

--- run.py
+++ run.py
@@ -1,6 +1,22 @@
 import requests
-url = 'http://capsuleshow.com/brands?page=1&view=list'
+from lxml import html
 
+all_len = []
+all_field = []
+
+
+base_url = 'http://capsuleshow.com'
+url = 'http://capsuleshow.com/brands?page=1&view=list'
 r = requests.get(url)
-with open('test.html', 'w') as output_file:
-  output_file.write(r.text)
+tree = html.fromstring(r.content)
+links = tree.xpath("//*[@id='brand-list']//a/@href")
+
+for profile in links:
+    r = requests.get(base_url+profile)
+    tree = html.fromstring(r.content)
+    # name = tree.xpath("//h2/text()")
+    # where = tree.xpath("//p[@class='country']//text()")
+    all_len.append(len(tree.xpath("//h6/text()")))
+    all_field.append(tree.xpath("//h6/text()"))
+
+
