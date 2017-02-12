ca578e68bd0f09dce63e29149eb417a4b2c576c8
Sergey <sabarovsky@yahoo.com>
2017-02-12 12:04:36 +0200

    part scrape ready code

--- run.py
+++ run.py
@@ -29,14 +29,39 @@ for profile in all_link:
     name = soup.find('h2').text.strip()
     where = soup.find('p',{'class', 'country'}).text.strip()
     description = soup.find("blockquote").text.strip()
-    contact_info = soup.find('h6')
     allh6 = soup.findAll('h6')
-    if contact_info.text == 'Contact information:':
+    contact_info = allh6[0]
+    sales = allh6[1]
+    add_sales = allh6[2]
+    pr = allh6[3]
+    if allh6[0].text == 'Contact information:':
         ci1 = contact_info.nextSibling.nextSibling.text.strip()
         ci2 = contact_info.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
         ci3 = contact_info.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
         ci4 = contact_info.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
         ci5 = contact_info.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        
+    if allh6[1].text == 'Sales:':
+        d1 = sales.nextSibling.nextSibling.text.strip()
+        d2 = sales.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        d3 = sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        d4 = sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        d5 = sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        d6 = sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+    if allh6[2].text == 'Additional Sales Contact:':
+        e1 = add_sales.nextSibling.nextSibling.text.strip()
+        e2 = add_sales.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        e3 = add_sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        e4 = add_sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        e5 = add_sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        e6 = add_sales.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()        
+    if allh6[3].text == 'Marketing and PR:':
+        f1 = pr.nextSibling.nextSibling.text.strip()
+        f2 = pr.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        f3 = pr.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        f4 = pr.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        f5 = pr.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()
+        f6 = pr.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.text.strip()                
     all_len.append(len(tree.xpath("//h6/text()")))#max 6
     all_field.append(tree.xpath("//h6/text()"))
     for i in all_field:
